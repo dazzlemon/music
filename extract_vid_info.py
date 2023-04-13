ee4762d8fdb480d5d78d7f26509f484e43829ea9
dazzlemon <daanill13470@gmail.com>
2023-04-13 19:53:56 +0300

    improve vid info extraction

--- extract_vid_info.py
+++ extract_vid_info.py
@@ -4,25 +4,25 @@ INPUT_FILE contains urls of youtube videos (1 per line)
 OUTPUT_FILE will contain records of youtube video urls, author and title
 1 per line sorted by authors
 """
+from sys import stdin
 from pytube import YouTube
 from pytube.exceptions import PytubeError
 
-INPUT_FILE = 'to_download2.log'
-OUTPUT_FILE = 'to_download3.log'
-
-def yt_url_to_datastring(url):
+def ytvid_author_and_title(url):
     """
-    maps youtube video url to string in format: 'url | author | title\n'
+    returns author and title of youtube video by its link
     """
     try:
-        yt_vid = YouTube(url.strip())
-
-        return f'{url.strip()} | {yt_vid.author} | {yt_vid.title}\n'
+        yt_vid = YouTube(url)
+        return (yt_vid.author, yt_vid.title)
     except PytubeError:
-        return f'{url.strip()} |  |  \n'
+        return (None, None)
 
-with open(INPUT_FILE, encoding='utf-8') as input_file:
-    yt_urls = input_file.readlines()
-    yt_records = map(yt_url_to_datastring, yt_urls)
-    with open(OUTPUT_FILE, 'w+', encoding='utf-8') as output_file:
-        output_file.writelines(yt_records)
+for yt_url in map(lambda u: u.strip(), stdin):
+    (author, title) = ytvid_author_and_title(yt_url)
+    print(yt_url)
+    if author is not None:
+        print(author)
+    if title is not None:
+        print(title)
+    print()
