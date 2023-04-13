dd295e4b3d74acc0031107feff80852b1ae8f130
dazzlemon <daanill13470@gmail.com>
2023-04-13 19:59:40 +0300

    no unnecessary function

--- extract_vid_info.py
+++ extract_vid_info.py
@@ -9,22 +9,12 @@ from sys import stdin
 from pytube import YouTube
 from pytube.exceptions import PytubeError
 
-def ytvid_author_and_title(url):
-    """
-    Returns author and title of youtube video by its link.
-    Both can be None
-    """
-    try:
-        yt_vid = YouTube(url)
-        return (yt_vid.author, yt_vid.title)
-    except PytubeError:
-        return (None, None)
-
 for yt_url in map(lambda u: u.strip(), stdin):
-    (author, title) = ytvid_author_and_title(yt_url)
     print(yt_url)
-    if author is not None:
-        print(author)
-    if title is not None:
-        print(title)
+    try:
+        yt_vid = YouTube(yt_url)
+        print(yt_vid.author)
+        print(yt_vid.title)
+    except PytubeError:
+        pass
     print()
