b67097c38325cc07d8f01bafd70d12ab5f67d6c1
dazzlemon <daanill13470@gmail.com>
2023-04-14 20:31:57 +0300

    fully replaced by jq

--- get_links.sh
+++ get_links.sh
@@ -1,21 +1,23 @@
 my_jq_expression='
-    group_by(.uploader)
+  group_by(.uploader)
   | map({
       uploader: .[0].uploader,
       videos: [.[]
-              | "https://youtu.be/\(.id) \(.title | @sh)"
+              | "https://youtu.be/\(.id) \(.title)"
               ]
     })
   | sort_by(-(.videos | length))
   | .[]
-  | "\(.uploader)\n\(.videos[])\n"
+  | "\(.uploader)\n"
+  + (.videos | join("\n"))
+  + "\n"
 '
 
 youtube-dl \
   -j \
-	--flat-playlist \
-	"https://www.youtube.com/playlist?list=PLmpL10r_giI1ipR20I16Qwa3lEfENtYPv" \
+  --flat-playlist \
+  "https://www.youtube.com/playlist?list=PLmpL10r_giI1ipR20I16Qwa3lEfENtYPv" \
 | jq \
-    -s \
-    "$my_jq_expression"
-
+  -s \
+	-r \
+  "$my_jq_expression"
