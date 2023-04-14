2b865a5bfe30bab81d8db34583eec2038ad34a14
dazzlemon <daanill13470@gmail.com>
2023-04-14 20:21:33 +0300

    almost replaced by jq

--- get_links.sh
+++ get_links.sh
@@ -1,1 +1,21 @@
-youtube-dl -j --flat-playlist "https://www.youtube.com/playlist?list=PLmpL10r_giI1ipR20I16Qwa3lEfENtYPv" | jq -r '.id' | sed 's_^_https://youtu.be/_'
+my_jq_expression='
+    group_by(.uploader)
+  | map({
+      uploader: .[0].uploader,
+      videos: [.[]
+              | "https://youtu.be/\(.id) \(.title | @sh)"
+              ]
+    })
+  | sort_by(-(.videos | length))
+  | .[]
+  | "\(.uploader)\n\(.videos[])\n"
+'
+
+youtube-dl \
+  -j \
+	--flat-playlist \
+	"https://www.youtube.com/playlist?list=PLmpL10r_giI1ipR20I16Qwa3lEfENtYPv" \
+| jq \
+    -s \
+    "$my_jq_expression"
+
