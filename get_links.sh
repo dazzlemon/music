34dc71523e8a79af6967389fef4dcefae9989539
dazzlemon <daanill13470@gmail.com>
2023-04-14 22:32:31 +0300

    even shorter

--- get_links.sh
+++ get_links.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-if [[ $# -ne 1 ]]; then
-  echo "Usage: $(basename "$0") playlist_link" >&2
-  exit 1
-fi
-
+[[ $# -ne 1 ]] && echo 'Usage: $(basename $0) playlist_link' >&2 && exit 1
 youtube-dl -j --flat-playlist "$1" | jq -sr '
   group_by(.uploader) |
   map({
