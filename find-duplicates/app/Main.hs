2ea2a690869ebfb1509a6b2bf7534c309edd382e
dazzlemon <daanill13470@gmail.com>
2023-04-22 20:05:09 +0300

    find-duplicates done, links updated

---
+++ find-duplicates/app/Main.hs
@@ -0,0 +1,26 @@
+module Main (main) where
+
+import Data.List (isPrefixOf)
+import Control.Monad (guard)
+import Data.List.Extra (sortOn)
+import Text.EditDistance (levenshteinDistance, defaultEditCosts)
+
+pairs :: [a] -> [(a, a)]
+pairs xs = do
+  (x, i) <- zip xs [0..]
+  (y, j) <- zip xs [0..]
+  guard (i < j)
+  return (x, y)
+
+main :: IO ()
+main = getContents
+     >>= putStrLn
+       . unlines
+       . take 2000
+       . map (\(x, y) -> x ++ "\n" ++ y ++ "\n\n")
+       . sortOn (uncurry (levenshteinDistance defaultEditCosts))
+       . pairs
+       . map (drop 29)
+       . filter ("https://youtu.be/" `isPrefixOf`)
+       . lines
+
