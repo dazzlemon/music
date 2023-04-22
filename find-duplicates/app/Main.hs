9ca7a3ca884ebe579153e9255ad2e3aba27f6619
dazzlemon <daanill13470@gmail.com>
2023-04-22 22:15:08 +0300

    good enough

--- find-duplicates/app/Main.hs
+++ find-duplicates/app/Main.hs
@@ -13,9 +13,9 @@ pairs xs = [ (x, y) | (x, i) <- zip xs [0..]
                     ]
 
 extractTitle :: String -> Maybe String
-extractTitle line = do
-  [[_, title]] <- pure $ line =~ "^https://youtu.be/[A-Za-z0-9]+ (.+)$"
-  return title
+extractTitle line = case line =~ "^https://youtu.be/[A-Za-z0-9]+ (.+)$" of
+  [[_, title]] -> Just title
+  _            -> Nothing
 
 main :: IO ()
 main = lineInteract $ take 2000
