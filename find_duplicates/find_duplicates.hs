8d1fe38d9ae8aa046981d21324e103d5fe8870b1
dazzlemon <daanill13470@gmail.com>
2023-04-22 18:52:41 +0300

    almost done

--- find_duplicates/find_duplicates.hs
+++ find_duplicates/find_duplicates.hs
@@ -1,8 +1,19 @@
 import Data.List (isPrefixOf)
+import Control.Monad (guard)
+
+pairs :: [a] -> [(a, a)]
+pairs xs = do
+  (x, i) <- zip xs [0..]
+  (y, j) <- zip xs [0..]
+  guard (i < j)
+  return (x, y)
 
 main :: IO ()
 main = getContents
      >>= putStrLn
        . unlines
-       . filter (isPrefixOf "https")
+       . map (\(x, y) -> x ++ "\n" ++ y ++ "\n\n")
+       . pairs
+       . map (drop 29)
+       . filter ("https://youtu.be/" `isPrefixOf`)
        . lines
